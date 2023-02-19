0f479543c116b62b89a3e0e0053f8585c7541727
GGerginov <electro44@abv.bg>
2023-02-19 15:11:36 +0200

    Create a Login component

---
+++ src/components/login/Login.js
@@ -0,0 +1,26 @@
+
+import styles from "./Login.module.scss"
+import {InputAdornment, TextField} from "@mui/material";
+import Button from "@mui/material/Button";
+import EmailIcon from '@mui/icons-material/Email';
+import LockIcon from '@mui/icons-material/Lock';
+
+const Login = ()=>{
+
+
+    return <div className={styles['login-form']}>
+
+
+        <TextField InputProps={{
+            startAdornment: <InputAdornment position="start"><EmailIcon/></InputAdornment>,
+        }}/>
+        <TextField InputProps={{
+            startAdornment: <InputAdornment position="start"><LockIcon/></InputAdornment>,
+        }}/>
+
+        <Button variant={'contained'} size={"large"} className={styles.button}>Login</Button>
+    </div>
+
+}
+
+export default Login
